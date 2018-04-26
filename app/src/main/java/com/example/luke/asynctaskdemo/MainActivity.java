ccf798777743e47cd90ab7e543bec41a079729cc
devnoteovh <lukaszkwasniewicz@gmail.com>
2018-04-26 15:19:36 +0200

    Access to mobile internet has been added

--- app/src/main/java/com/example/luke/asynctaskdemo/MainActivity.java
+++ app/src/main/java/com/example/luke/asynctaskdemo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.luke.asynctaskdemo;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,9 +14,11 @@ import android.widget.TextView;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //setMobileDataEnabled(getApplicationContext(),true);
+
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         progressBar.setMax(10);
         btn = (Button) findViewById(R.id.btn);
@@ -39,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 count = 1;
                 progressBar.setVisibility(View.VISIBLE);
                 progressBar.setProgress(0);
-                switch (v.getId()){
+                switch (v.getId()) {
                     case R.id.btn:
                         new MyTask().execute(10);
                         break;
@@ -47,15 +53,19 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         btn.setOnClickListener(listener);
+        //setMobileDataEnabled(getApplicationContext(),true);
     }
 
-    class MyTask extends AsyncTask<Integer,Integer,String> {
+
+
+    class MyTask extends AsyncTask<Integer, Integer, String> {
 
         StringBuilder result;
+
         protected String doInBackground(Integer... params) {
 
 
-             result = new StringBuilder();
+            result = new StringBuilder();
 
             for (; count <= params[0]; count++) {
                 try {
@@ -71,7 +81,6 @@ public class MainActivity extends AppCompatActivity {
                 }
 
 
-
             }
             try {
                 sendPost();
@@ -81,21 +90,24 @@ public class MainActivity extends AppCompatActivity {
             }
             return result.toString();
         }
-            protected void onPostExecute (String result){
-                progressBar.setVisibility(View.GONE);
-                txt.setText(result);
-                btn.setText("Restart");
-            }
-            protected void onPreExecute () {
-                txt.setText("Task Starting...");
-            }
-            protected void onProgressUpdate (Integer...values){
-                txt.setText("Running.." + /*values[0]*/ result.toString() );
-                progressBar.setProgress(values[0]);
-            }
 
-
+        protected void onPostExecute(String result) {
+            progressBar.setVisibility(View.GONE);
+            txt.setText(result);
+            btn.setText("Restart");
         }
+
+        protected void onPreExecute() {
+            txt.setText("Task Starting...");
+        }
+
+        protected void onProgressUpdate(Integer... values) {
+            txt.setText("Running.." + /*values[0]*/ result.toString());
+            progressBar.setProgress(values[0]);
+        }
+
+
+    }
 
 
     private void sendPost() throws Exception {
@@ -117,9 +129,9 @@ public class MainActivity extends AppCompatActivity {
         wr.close();
 
         int responseCode = con.getResponseCode();
-        Log.i("SENDING","\nSending 'POST' request to URL : " + url);
-        Log.i("SENDING","Post parameters : " + urlParameters);
-        Log.i("SENDING","Response Code : " + responseCode);
+        Log.i("SENDING", "\nSending 'POST' request to URL : " + url);
+        Log.i("SENDING", "Post parameters : " + urlParameters);
+        Log.i("SENDING", "Response Code : " + responseCode);
 
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(con.getInputStream()));
@@ -132,7 +144,7 @@ public class MainActivity extends AppCompatActivity {
         in.close();
 
         //print result
-        Log.i("WYNIK POST ",response.toString());
+        Log.i("WYNIK POST ", response.toString());
         con.disconnect();
     }
 
@@ -143,10 +155,38 @@ public class MainActivity extends AppCompatActivity {
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("DELETE");
         int responseCode = con.getResponseCode();
-        Log.i("DELETE","\nSending 'DELETE' request to URL : " + url);
-        Log.i("DELETE","Response Code : " + responseCode);
+        Log.i("DELETE", "\nSending 'DELETE' request to URL : " + url);
+        Log.i("DELETE", "Response Code : " + responseCode);
         con.disconnect();
 
     }
+
+    private void setMobileDataEnabled(Context context, boolean enabled) {
+        try {
+            final ConnectivityManager conman = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            final Class conmanClass = Class.forName(conman.getClass().getName());
+            final Field iConnectivityManagerField = conmanClass.getDeclaredField("mService");
+            iConnectivityManagerField.setAccessible(true);
+            final Object iConnectivityManager = iConnectivityManagerField.get(conman);
+            final Class iConnectivityManagerClass = Class.forName(iConnectivityManager.getClass().getName());
+            final Method setMobileDataEnabledMethod = iConnectivityManagerClass.getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
+            setMobileDataEnabledMethod.setAccessible(true);
+
+            setMobileDataEnabledMethod.invoke(iConnectivityManager, enabled);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
 
